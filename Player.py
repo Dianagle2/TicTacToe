6e7925bf1370f28cad8dc9b88b14ab9012946902
Diana Mazenko <diana.9299@gmail.com>
2020-09-05 21:45:03 +0300

    The class of the Player

---
+++ Player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self, name, symbol):
+        self.name = name
+        self.symbol = symbol
+
+    def get_name(self):
+        return self.name
+    
+    def get_symbol(self):
+        return self.symbol
+    
+    def ask_move(self):
+        raise NotImplementedError
